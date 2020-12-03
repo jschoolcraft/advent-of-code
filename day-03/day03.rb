2a7bb36be451f1bea957fcef73e718cc8b9d391e
Jeff Schoolcraft <jeff@thequeue.net>
2020-12-03 08:20:34 -0500

    Day 3 solution

---
+++ day-03/day03.rb
@@ -0,0 +1,69 @@
+require "byebug"
+
+module Year2020
+  class Day03
+#     ..##.......
+#     #...#...#..
+#     .#....#..#.
+#     ..#.#...#.#
+#     .#...##..#.
+#     ..#.##.....
+#     .#.#.#....#
+#     .#........#
+#     #.##...#...
+#     #...##....#
+#     .#..#...#.#
+    def part1(input)
+      landscape = input.map(&:chars)
+
+      down = 1
+      right = 3
+      trees = 0
+
+      index = 0
+      0.step(landscape.size - 1, down) do |depth|
+        trees += 1 if has_tree?(landscape[depth], depth, right, index)
+        index += 1
+      end
+      trees
+    end
+
+    def part2(input)
+      landscape = input.map(&:chars)
+
+      slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2],
+      ]
+
+      all_trees = []
+
+      slopes.each do |right, down|
+        trees = 0
+        index = 0
+        0.step(landscape.size - 1, down) do |depth|
+          trees += 1 if has_tree?(landscape[depth], depth, right, index)
+          index += 1
+        end
+        all_trees << trees
+      end
+
+      puts
+      puts all_trees.inspect
+      puts
+      all_trees.reduce(&:*)
+    end
+
+    def has_tree?(row, depth, right, i)
+      index = i * right
+
+      if index >= row.size
+        index = index % row.size
+      end
+      row[index] == "#"
+    end
+  end
+end
---
+++ day-03/test_day03.rb
@@ -0,0 +1,60 @@
+require "minitest/autorun"
+require "./day03"
+
+class TestDay03 < Minitest::Test
+  def setup
+    @solution = Year2020::Day03.new
+  end
+
+  def test_it_works
+    assert true
+  end
+
+  def test_sample_part_1
+    input = <<~EOF
+      ..##.......
+      #...#...#..
+      .#....#..#.
+      ..#.#...#.#
+      .#...##..#.
+      ..#.##.....
+      .#.#.#....#
+      .#........#
+      #.##...#...
+      #...##....#
+      .#..#...#.#
+    EOF
+
+    assert_equal 7, @solution.part1(input.split)
+  end
+
+  def test_part_1
+    input = File.read("./input.txt").split
+
+    assert_equal 176, @solution.part1(input)
+  end
+
+  def test_sample_part_2
+    input = <<~EOF
+      ..##.......
+      #...#...#..
+      .#....#..#.
+      ..#.#...#.#
+      .#...##..#.
+      ..#.##.....
+      .#.#.#....#
+      .#........#
+      #.##...#...
+      #...##....#
+      .#..#...#.#
+    EOF
+
+    assert_equal 336, @solution.part2(input.split)
+  end
+
+  def test_part_2
+    input = File.read("./input.txt").split
+
+    assert_equal 4242, @solution.part2(input)
+  end
+end
