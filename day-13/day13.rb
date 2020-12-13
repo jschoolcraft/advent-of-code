067b17df609eecf5962c31609ebc48b94b8cceb7
Jeff Schoolcraft <jeff@thequeue.net>
2020-12-13 14:02:10 -0500

    Day 13 part 2
    Got some help with the implementation of the Chinese remainder theorem
    from: https://rosettacode.org/wiki/Chinese_remainder_theorem#Ruby

--- day-13/day13.rb
+++ day-13/day13.rb
@@ -6,7 +6,6 @@ module Year2020
       start_time = input.shift.to_i
       bus_ids = input.shift.split(/,/).reject { |i| i == "x" }.map(&:to_i).sort
 
-      # puts format("start_time: %s, bus_ids: %s", start_time, bus_ids.inspect)
 
       product = 0
       start_time.upto(start_time + bus_ids.first) do |i|
@@ -14,69 +13,41 @@ module Year2020
         if departing.any?
           bus_id = departing.first
           product = (i - start_time) * bus_id
-          # puts format("time: %s, bus: %s, product: %s", i, bus_id, product)
           break
         end
       end
       product
     end
 
-    def part2(input)
-      puts
-      bus_ids = input.last.split(/,/)
-
-      puts format("bus_ids: %s", bus_ids.inspect)
-
-      bus_offsets = []
-      bus_ids.each_with_index do |b, i|
-        next if b.to_i == 0
-
-        bus_offsets << [i, b.to_i]
+    def extended_gcd(a, b)
+      last_remainder, remainder = a.abs, b.abs
+      x, last_x, y, last_y = 0, 1, 1, 0
+      while remainder != 0
+        last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
+        x, last_x = last_x - quotient * x, x
+        y, last_y = last_y - quotient * y, y
       end
-      # bus_offsets.shift
-      puts format("bus_offsets: %s", bus_offsets.inspect)
-
-      n = bus_offsets.map(&:last).reduce(:*)
-      puts format("N: %s", n)
-
-      ns = []
-      0.upto(bus_offsets.size - 1) do |i|
-        f = bus_offsets.select { |b| bus_offsets.index(b) != i }.map(&:last)
-        puts format("i: %s, rest: %s", i, f.inspect)
-        ns << f.reduce(:*)
-      end
-      puts format("Ns: %s", ns.inspect)
-
-      xs = []
-      ns.each_with_index do |n, i|
-        x = n % bus_offsets[i].last
-        y = solve(x, 1, bus_offsets[i].last)
-        xs << y
-      end
-      puts format("Xs: %s", xs.inspect)
-
-      xnbs = []
-      0.upto(xs.size - 1) do |i|
-        puts format("%s * %s * %s", ns[i], xs[i], bus_offsets[i].first)
-        foo = ns[i] * xs[i] * bus_offsets[i].first
-        puts format("foo: %s", foo)
-        xnbs << ns[i] * xs[i] * bus_offsets[i].first
-      end
-      puts format("Xbns: %s", xnbs.inspect)
-      x = xnbs.sum
-      puts x
-      puts format("x mod n: %s", x % n)
-      x % n
-      # solve(1, x % n, n)
+      return last_remainder, last_x * (a < 0 ? -1 : 1)
     end
 
-    def solve(x, y, z)
-      puts format("x: %s, y: %s, z: %s", x, y, z)
-      i = 0
-      loop do
-        i += 1
-        return i if (x * i) % z == y
+    def invmod(e, et)
+      g, x = extended_gcd(e, et)
+      raise 'Multiplicative inverse modulo does not exist!' if g != 1
+      x % et
+    end
+
+    def part2(input)
+      # stolen from https://rosettacode.org/wiki/Chinese_remainder_theorem#Ruby
+      # after getting a hint that this actually uses the Chinese remainder theorem
+      bus_ids = input.last.split(/,/).map { |b| b.to_i if b.to_i != 0 }
+
+      foo = bus_ids.each.with_index.map { |b, i| [b, (i * -1) % b] if !b.nil? }.reject { |s| s.nil? }
+
+      max = foo.map(&:first).reduce(:*)
+      series = foo.map do |m, r|
+        r * max * invmod(max / m, m) / m
       end
+      series.sum % max
     end
   end
 end
--- day-13/test_day13.rb
+++ day-13/test_day13.rb
@@ -30,24 +30,6 @@ class TestDay13 < Minitest::Test
     assert_equal 1068781, @solution.part2(input.split(/\n/))
   end
 
-  def test_yt_sample
-    input = <<~EOF
-      939
-      7,5,4,9,x,x,x,13
-    EOF
-
-    assert_equal 1866, @solution.part2(input.split(/\n/))
-  end
-
-  def test_tecosaur_sample
-    input = <<~EOF
-      939
-      7,7,x,11,x,x,x,x,13
-    EOF
-
-    assert_equal 190, @solution.part2(input.split(/\n/))
-  end
-
   def test_sample_part_2_1
     input = <<~EOF
       939
@@ -79,7 +61,6 @@ class TestDay13 < Minitest::Test
     input = <<~EOF
       939
       67,7,x,59,61
-      67,7,59,61
     EOF
 
     assert_equal 1261476, @solution.part2(input.split(/\n/))
@@ -97,6 +78,6 @@ class TestDay13 < Minitest::Test
   def test_part_2
     input = File.read("./input.txt").split
 
-    assert_equal 4242, @solution.part2(input)
+    assert_equal 526090562196173, @solution.part2(input)
   end
 end
