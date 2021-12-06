b7e0bb26fee5a71412c031f8659729a069ed4e79
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2021-12-06 09:02:32 -0500

    Leave a pointer to the inital implementation Day6

--- 2021/day-06/day06.rb
+++ 2021/day-06/day06.rb
@@ -2,6 +2,11 @@ require "byebug"
 
 module Year2021
   class Day06
+    # This wasn't my first solution, my first solution just kept
+    # building the array and that didn't work for part 2.
+    #
+    # You can see where I started here:
+    # https://github.com/jschoolcraft/advent-of-code/commit/c6d43030f0d5cb353fbc3a420b48b0ab2c4bd89a
     def part1(input, days)
       fish = input.split(/,/).map(&:to_i)
 
