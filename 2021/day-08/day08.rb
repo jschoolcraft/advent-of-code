27481f23035d640bf817428f87c288c96d47230a
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2021-12-08 08:30:53 -0500

    2021 Day 8 - Part 2 solution
    This seemed super tedious for some reason, I'm hoping after a coffee and
    some distance I'll see some more elegant solution but done is dun.

--- 2021/day-08/day08.rb
+++ 2021/day-08/day08.rb
@@ -10,8 +10,106 @@ module Year2021
         .size
     end
 
+    def find_segments(input)
+      line = input.strip.split(/\s+/)
+      # acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab
+      # ab      => 1
+      # eafb    => 4
+      # dab     => 7
+      # acedgfb => 8
+      #
+      #   0:      1:      2:      3:      4:
+      #  aaaa    ....    aaaa    aaaa    ....
+      # b    c  .    c  .    c  .    c  b    c
+      # b    c  .    c  .    c  .    c  b    c
+      #  ....    ....    dddd    dddd    dddd
+      # e    f  .    f  e    .  .    f  .    f
+      # e    f  .    f  e    .  .    f  .    f
+      #  gggg    ....    gggg    gggg    ....
+      #
+      #   5:      6:      7:      8:      9:
+      #  aaaa    aaaa    aaaa    aaaa    aaaa
+      # b    .  b    .  .    c  b    c  b    c
+      # b    .  b    .  .    c  b    c  b    c
+      #  dddd    dddd    ....    dddd    dddd
+      # .    f  e    f  .    f  e    f  .    f
+      # .    f  e    f  .    f  e    f  .    f
+      #  gggg    gggg    ....    gggg    gggg
+      #
+      segments = {}
+      one = line.detect { |s| s.size == 2 }
+      four = line.detect { |s| s.size == 4 }
+      seven = line.detect { |s| s.size == 3 }
+      eight = line.detect { |s| s.size == 7 }
+
+      three = line.select { |s| s.size == 5 }.detect { |s| s.chars.include?(one.chars.first) && s.chars.include?(one.chars.last) }
+
+      # puts format("one: %s, three: %s, four: %s, seven: %s, eight: %s", one, three, four, seven, eight)
+
+      # p line.select { |s| s.size == 5 && s != three }
+      candidates = line.select { |s| s.size == 5 && s != three }.map { |s| s.chars - four.chars - seven.chars }
+      segments["e"] = (candidates.detect { |c| c.size == 2 } - candidates.detect { |c| c.size == 1 }).first
+
+      segments["f"] = line.select { |s| s.size == 6 }.select { |c| (c.chars - one.chars).size == 5 }.first.chars.detect { |c| one.chars.include? c }
+
+      segments["c"] = one.gsub(Regexp.new(segments["f"]), '')
+      segments["a"] = (seven.chars - one.chars).first
+      segments["g"] = (three.chars - seven.chars - four.chars).first
+      segments["b"] = (four.chars - three.chars).first
+      segments["d"] = ((three.chars - seven.chars) - [segments["g"]]).first
+      segments
+    end
+
+    def segments_to_digit(segments, map)
+      sorted = segments.chars.sort.join
+      # puts
+      # puts format('segments: %s', segments)
+      # puts format('sorted: %s', sorted)
+      # puts format('map: %s', map)
+
+      zero  = [map['a'], map['b'], map['c'], map['e'], map['f'], map['g']].sort.join
+      one   = [map['c'], map['f']].sort.join
+      two   = [map['a'], map['c'], map['d'], map['e'], map['g']].sort.join
+      three = [map['a'], map['c'], map['d'], map['f'], map['g']].sort.join
+      four  = [map['b'], map['c'], map['d'], map['f']].sort.join
+      five  = [map['a'], map['b'], map['d'], map['f'], map['g']].sort.join
+      six   = [map['a'], map['b'], map['d'], map['e'], map['f'], map['g']].sort.join
+      seven = [map['a'], map['c'], map['f']].sort.join
+      eight = [map['a'], map['b'], map['c'], map['d'], map['e'], map['f'], map['g']].sort.join
+      nine  = [map['a'], map['b'], map['c'], map['d'], map['f'], map['g']].sort.join
+
+      case sorted
+      when zero
+        0
+      when one
+        1
+      when two
+        2
+      when three
+        3
+      when four
+        4
+      when five
+        5
+      when six
+        6
+      when seven
+        7
+      when eight
+        8
+      when nine
+        9
+      end
+    end
+
+    def process_output(input)
+      signal, output = input.split(/\|/)
+      mappings = find_segments(signal)
+      output.strip.split(/\s+/).map { |o| segments_to_digit(o, mappings) }.join.to_i
+    end
+
     def part2(input)
-      4242
+      input.map { |i| process_output(i) }.sum
     end
   end
 end
--- 2021/day-08/test_day08.rb
+++ 2021/day-08/test_day08.rb
@@ -29,18 +29,70 @@ class TestDay08 < Minitest::Test
     assert_equal 274, @solution.part1(input)
   end
 
+  def test_find_segments
+    segments = @solution.find_segments("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab ")
+
+    assert_equal "d", segments["a"]
+    assert_equal "e", segments["b"]
+    assert_equal "a", segments["c"]
+    assert_equal "f", segments["d"]
+    assert_equal "g", segments["e"]
+    assert_equal "b", segments["f"]
+    assert_equal "c", segments["g"]
+  end
+
+  def test_segments_to_digits
+    mappings = {"e"=>"g", "f"=>"b", "c"=>"a", "a"=>"d", "g"=>"c", "b"=>"e", "d"=>"f"}
+    assert_equal 5, @solution.segments_to_digit("cdfeb", mappings)
+    assert_equal 3, @solution.segments_to_digit("fcadb", mappings)
+    assert_equal 3, @solution.segments_to_digit("cdbaf", mappings)
+  end
+
+  def test_process_output
+    input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    assert_equal 5353, @solution.process_output(input)
+    input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+    assert_equal 8394, @solution.process_output(input)
+    input = "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"
+    assert_equal 9781, @solution.process_output(input)
+    input = "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg"
+    assert_equal 1197, @solution.process_output(input)
+    input = "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
+    assert_equal 9361, @solution.process_output(input)
+    input = "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"
+    assert_equal 4873, @solution.process_output(input)
+    input = "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"
+    assert_equal 8418, @solution.process_output(input)
+    input = "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"
+    assert_equal 4548, @solution.process_output(input)
+    input = "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"
+    assert_equal 1625, @solution.process_output(input)
+    input = "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"
+    assert_equal 8717, @solution.process_output(input)
+    input = "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+    assert_equal 4315, @solution.process_output(input)
+  end
+
   def test_sample_part_2
-    skip
     input = <<~EOF
+      be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+      edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+      fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+      fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+      aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+      fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+      dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+      bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+      egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+      gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     EOF
 
-    assert_equal 4242, @solution.part2(input.split(/\n/))
+    assert_equal 61229, @solution.part2(input.split(/\n/))
   end
 
   def test_part_2
-    skip
     input = File.read("./input.txt").split(/\n/)
 
-    assert_equal 4242, @solution.part2(input)
+    assert_equal 1012089, @solution.part2(input)
   end
 end
