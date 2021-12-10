e91fac80c93688edce3c2b98f2aacf442922b5ff
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2021-12-10 08:24:35 -0500

    2021 Day 10 Final Solution

--- 2021/day-10/day10.rb
+++ 2021/day-10/day10.rb
@@ -2,14 +2,19 @@ require "byebug"
 
 module Year2021
   class Day10
+    CLOSERS = {
+      '(': ')',
+      '[': ']',
+      '{': '}',
+      '<': '>',
+    }.freeze
+
     def closes(open, close)
-      closers = {
-        '(': ')',
-        '[': ']',
-        '{': '}',
-        '<': '>',
-      }
-      closers[open.to_sym] == close
+      # puts format("open: %s, closes: %s", open, close)
+      return false if open.nil? || close.nil?
+      return false unless CLOSERS.keys.include?(open.to_sym)
+
+      CLOSERS[open.to_sym] == close
     end
 
     def score(errors)
@@ -22,34 +27,86 @@ module Year2021
       errors.map { |e| points[e.to_sym] }.sum
     end
 
-    def part1(input)
+    def parse_line(line)
       errors = []
+      parsed = []
+      found_error = false
+      # puts format("line: %s", line.chars)
+      line.chars.each do |char|
+        # puts format("char: %s", char)
+        next if found_error
 
-      input.each do |line|
-        parse = []
-        found_error = false
-        # puts format("line: %s", line.chars)
-        line.chars.each do |char|
-          # puts format("char: %s", char)
-          next if found_error
-
-          # puts format("parse: %s", parse)
-          # puts format("errors: %s", errors)
-
-          if ['(', '[', '{', '<'].include?(char)
-            parse << char
-          elsif closes(parse.last, char)
-            parse.pop
-          else
-            errors << char
-            found_error = true
-          end
+        if ['(', '[', '{', '<'].include?(char)
+          parsed << char
+        elsif closes(parsed.last, char)
+          parsed.pop
+        else
+          errors << char
+          found_error = true
         end
       end
-      score(errors)
+      [parsed, errors]
+    end
+
+    def close_line(chars, closers = [], pending = [])
+      # puts format("chars: %s, pending: %s, closers: %s", chars.join, pending, closers.join)
+      return closers if chars.size.zero?
+
+      if closes(chars[-2], chars[-1])
+        chars.pop
+        chars.pop
+      elsif closes(chars.last, pending.first)
+        chars.pop
+        pending.shift
+      elsif [')', ']', '>', '}'].include?(chars.last)
+        pending.unshift(chars.pop)
+      else
+        closers << CLOSERS[chars.pop.to_sym]
+      end
+      close_line(chars, closers, pending)
+    end
+
+    def closing_score(closers)
+      score = 0
+      closers.each do |closer|
+        score *= 5
+        score += case closer.to_sym
+                 when :")"
+                   1
+                 when :"]"
+                   2
+                 when :"}"
+                   3
+                 when :">"
+                   4
+                 end
+      end
+      score
+    end
+
+    def part1(input)
+      errors = []
+      input.each do |line|
+        errors << parse_line(line).last
+      end
+      score(errors.flatten)
     end
 
     def part2(input)
+      incomplete = []
+      input.each do |line|
+        incomplete << line if parse_line(line).last.empty?
+      end
+
+      # p incomplete
+
+      closings = []
+      incomplete.each do |line|
+        closings << close_line(line.chars)
+      end
+
+      closing_scores = closings.map { |c| closing_score(c) }
+      closing_scores.sort[closing_scores.size / 2]
     end
   end
 end
--- 2021/day-10/test_day10.rb
+++ 2021/day-10/test_day10.rb
@@ -14,6 +14,8 @@ class TestDay10 < Minitest::Test
 
     refute @solution.closes("[", ")")
     refute @solution.closes("[", ">")
+    refute @solution.closes("[", nil)
+    refute @solution.closes(">", nil)
   end
 
   def test_sample_part_1
@@ -39,6 +41,22 @@ class TestDay10 < Minitest::Test
     assert_equal 392097, @solution.part1(input)
   end
 
+  def test_close_line
+    # assert_equal '}}]])})]'.chars, @solution.close_line('[({(<(())[]>[[{[]{<()<>>'.chars)
+    # assert_equal ')}>]})'.chars, @solution.close_line('[(()[<>])]({[<{<<[]>>('.chars)
+    # assert_equal '}}>}>))))'.chars, @solution.close_line('(((({<>}<{<{<>}{[]{[]{}'.chars)
+    assert_equal ']]}}]}]}>'.chars, @solution.close_line('{<[[]]>}<{[{[{[]{()[[[]'.chars)
+    # assert_equal '])}>'.chars, @solution.close_line('<{([{{}}[<[[[<>{}]]]>[]]'.chars)
+  end
+
+  def test_closing_score
+    assert_equal 294, @solution.closing_score("])}>".chars)
+    assert_equal 288957, @solution.closing_score("}}]])})]".chars)
+    assert_equal 5566, @solution.closing_score(")}>]})".chars)
+    assert_equal 1480781, @solution.closing_score("}}>}>))))".chars)
+    assert_equal 995444, @solution.closing_score("]]}}]}]}>".chars)
+  end
+
   def test_sample_part_2
     input = <<~EOF
       [({(<(())[]>[[{[]{<()<>>
@@ -57,9 +75,8 @@ class TestDay10 < Minitest::Test
   end
 
   def test_part_2
-    skip
     input = File.read("./input.txt").split
 
-    assert_equal 4242, @solution.part2(input)
+    assert_equal 4263222782, @solution.part2(input)
   end
 end
