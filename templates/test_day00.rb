73e5ac3b451eae756ae18dabe04a58de743fbae2
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2024-12-04 11:49:14 -0500

    Update the template a bit
    Consolidate sample_data to a method.
    
    Fix actual input, mostly we want to `readlines` sometimes we don't, but
    we can change that.

--- templates/test_day00.rb
+++ templates/test_day00.rb
@@ -6,33 +6,32 @@ class TestDay<%= config[:day] %> < Minitest::Test
     @solution = Year<%= config[:year] %>::Day<%= config[:day] %>.new
   end
 
-  def test_it_works
-    assert true
-  end
-
   def test_sample_part_1
-    input = <<~EOF
-    EOF
+    input = sample_data
 
     assert_equal 42, @solution.part1(input.split(/\n/))
   end
 
   def test_part_1
-    input = File.read("./input.txt").split
+    input = File.readlines("./input.txt")
 
     assert_equal 42, @solution.part1(input)
   end
 
   def test_sample_part_2
-    input = <<~EOF
-    EOF
+    input = sample_data
 
     assert_equal 4242, @solution.part2(input.split(/\n/))
   end
 
   def test_part_2
-    input = File.read("./input.txt").split
+    input = File.readlines("./input.txt")
 
     assert_equal 4242, @solution.part2(input)
+  end
+
+  def sample_data
+    <<~EOF
+    EOF
   end
 end
